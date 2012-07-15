e2b8ab753797de492d463e707ca3e5823143fc97
Charles Brown <carbonrobot@gmail.com>
2012-07-15 13:33:21 -0500

    Added dialog namespace and methods

---
+++ Structure.Web/Scripts/defaults/dialogs.js
@@ -0,0 +1,58 @@
+﻿Structure.Dialogs = Structure.Dialogs || {};
+
+(Structure.Dialogs = function () {
+    
+    return {
+        Alert: function(msg){
+            var k = '<div class="modal">';
+            k += '<div class="modal-header"><button class="close" data-dismiss="modal">×</button><h3>Alert</h3></div>';
+            k += '<div class="modal-body"><p>' + msg + '</p></div>';
+            k += '<div class="modal-footer"><a href="#" class="btn" data-dismiss="modal">Close</a></div></div>';
+            $k = $(k).appendTo('body');
+
+            $k.modal({
+                keyboard: true
+            });
+        },
+
+        Confirm: function(msg, callback){
+            var k = '<div class="modal" style="display:none;">';
+            k += '<div class="modal-header"><h3>Confirm</h3></div>';
+            k += '<div class="modal-body"><p>' + msg + '</p></div>';
+            k += '<div class="modal-footer"><a href="#" class="btn" data-dismiss="modal">Close</a><a href="#" class="ok btn btn-primary">OK</a></div></div>';
+            $k = $(k).appendTo('body');
+
+            $k.on('show', function () {
+                var dialog = this;
+                $('a.ok').click(function (event) {
+                    event.preventDefault();
+                    if ($.isFunction(callback)) {
+                        callback.apply(this);
+                    }
+                    $(dialog).modal('hide');
+                });
+            });
+
+            $k.modal({
+                keyboard: false
+            });
+        },
+
+        Modal: function (href) {
+            if (href.indexOf('#') == 0) {
+                $(href).modal();
+            }
+            else {
+                Structure.Block();
+                $.get(href, function (data) {
+                    $(data).modal()
+                        .on('hidden', function () {
+                            $(this).remove();
+                        });
+                    Structure.Unblock();
+                });
+            }
+        }
+    };
+
+})();
